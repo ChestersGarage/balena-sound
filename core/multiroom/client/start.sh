97e6775f3d679aba99e35934930b57465bb1b42e
Mark Chester <mark@chesterfamily.org>
2020-12-04 20:06:53 -0800

    Initial changes pulled from PoC

--- core/multiroom/client/start.sh
+++ core/multiroom/client/start.sh
@@ -27,12 +27,14 @@ else
     SNAPCAST_CLIENT_ID=$(echo $SOUND_DEVICE_NAME | sed -e 's/[^A-Za-z0-9.-]/-/g')
 fi
 
+if [[ ! -z $OUTPUT_DEVICE ]]
+then
+    OUTPUT="--soundcard $OUTPUT_DEVICE"
+fi
+
 # Start snapclient
 if [[ "$MODE" == "MULTI_ROOM" || "$MODE" == "MULTI_ROOM_CLIENT" ]]; then
-  # Start snapclient and filter out those pesky chunk logs
-  # grep filter can be removed when we get snapcast v0.20
-  # see: https://github.com/badaix/snapcast/issues/559#issuecomment-615874719
-  /usr/bin/snapclient --host $SNAPSERVER $LATENCY --hostID $SNAPCAST_CLIENT_ID --logfilter *:notice
+  /usr/bin/snapclient --host $SNAPSERVER $LATENCY --hostID $SNAPCAST_CLIENT_ID $OUTPUT --logfilter *:notice
 else
   echo "Multi-room client disabled. Exiting..."
   exit 0
